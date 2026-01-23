f0454a4fba67f04b2a0d9d266ef249c796bb111c
Davi Santos <daavsantosl@gmail.com>
2026-01-23 03:42:42 -0300

    refactor: change invalidate queries function to utils

--- app/hooks/use-post-mutations.ts
+++ app/hooks/use-post-mutations.ts
@@ -6,36 +6,31 @@ import {
 	POSTS_TAG,
 } from "@/services/api/posts";
 import type { CreatePostData, UpdatePostData } from "@/services/api/posts";
+import { revalidateClientTags } from "@/services/utils";
 
 export function useCreatePost() {
-	const queryClient = useQueryClient();
-
 	return useMutation({
 		mutationFn: (data: CreatePostData) => createPost(data),
 		onSuccess: () => {
-			queryClient.invalidateQueries({ queryKey: [POSTS_TAG] });
+			revalidateClientTags([POSTS_TAG]);
 		},
 	});
 }
 
 export function useUpdatePost() {
-	const queryClient = useQueryClient();
-
 	return useMutation({
 		mutationFn: (data: UpdatePostData) => updatePost(data),
 		onSuccess: () => {
-			queryClient.invalidateQueries({ queryKey: [POSTS_TAG] });
+			revalidateClientTags([POSTS_TAG]);
 		},
 	});
 }
 
 export function useDeletePost() {
-	const queryClient = useQueryClient();
-
 	return useMutation({
 		mutationFn: (id: number) => deletePost(id),
 		onSuccess: () => {
-			queryClient.invalidateQueries({ queryKey: [POSTS_TAG] });
+			revalidateClientTags([POSTS_TAG]);
 		},
 	});
 }
